2a89b25f749599b8a870fbb34add69d24f5d124a
keshavjatt <thecoder780@gmail.com>
2025-02-14 20:02:42 +0530

    worked on ChatView page

--- app/(main)/workspace/[id]/page.jsx
+++ app/(main)/workspace/[id]/page.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 function Workspace() {
   return (
     <div className='p-10'>
-        <div className='grid grid-cols-1 md:grid-cols-3'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-10'>
             <ChatView />
             <div className='col-span-2'>
                 <CodeView />
--- components/custom/ChatView.jsx
+++ components/custom/ChatView.jsx
@@ -1,27 +1,49 @@
 "use client"
+import { MessagesContext } from '@/context/MessagesContext';
+import { UserDetailContext } from '@/context/UserDetailsContext';
 import { api } from '@/convex/_generated/api';
+import Colors from '@/data/Colors';
 import { useConvex } from 'convex/react';
+import Image from 'next/image';
 import { useParams } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 
 function ChatView() {
-    const {id}=useParams();
-    const convex=useConvex();
+    const {id} = useParams();
+    const convex = useConvex();
+    const {userDetail, setUserDetail}=useContext(UserDetailContext);
+    const {messages, setMessages} = useContext(MessagesContext);  // yaha setMessages use karo
 
     useEffect(()=>{
-        id&&GetWorkspaceData();
+        id && GetWorkspaceData();
     },[id])
 
     /** Used to Get Workspace data using Workspace ID **/
-    const GetWorkspaceData=async()=>{
-        const result=await convex.query(api.workspace.GetWorkspace, {
-            workspaceId:id
+    const GetWorkspaceData = async () => {
+        const result = await convex.query(api.workspace.GetWorkspace, {
+            workspaceId: id
         });
+        setMessages(result?.messages)  // Correct function use karo
         console.log(result);
     }
-  return (
-    <div>ChatView</div>
-  )
+
+    return (
+        <div>
+            <div>
+                {messages?.map((msg,index)=>(
+                  <div key={index} 
+                  className='p-3 rounded-lg mb-2'
+                  style={{
+                    backgroundColor:Colors.CHAT_BACKGROUND
+                  }}>
+                      {msg?.role=='user'&&
+                      <Image src={userDetail?.picture} alt='userImage' width={35} height={35} className='rounded-full'/>}
+                      <h2>{msg.content}</h2>
+                  </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
-export default ChatView
+export default ChatView;
