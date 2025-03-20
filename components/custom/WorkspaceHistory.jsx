385cbfcf1c4693bcf8b6bfa11ea62ac06f08a3a8
keshavjatt <thecoder780@gmail.com>
2025-03-20 12:33:34 +0530

    worked on sidebar

--- components/custom/WorkspaceHistory.jsx
+++ components/custom/WorkspaceHistory.jsx
@@ -2,11 +2,12 @@
 import { UserDetailContext } from '@/context/UserDetailsContext'
 import { api } from '@/convex/_generated/api';
 import { useConvex } from 'convex/react';
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 function WorkspaceHistory() {
     const { userDetail, setUserDetail } = useContext(UserDetailContext);
     const convex=useConvex();
+    const [workspaceList, setWorkspaceList] = useState();
 
     useEffect(()=>{
         userDetail&&GetAllWorkspace();
@@ -16,12 +17,19 @@ function WorkspaceHistory() {
         const result=await convex.query(api.workspace.GetAllWorkspace, {
             userId: userDetail?._id
         });
-
+        setWorkspaceList(result);
         console.log(result);
     }
   return (
     <div>
         <h2 className='font-medium text-lg'>Your Chats</h2>
+        <div>
+            {workspaceList&&workspaceList?.map((workspace, index)=>(
+                <h2 key={index} className='text-sm text-gray-400 mt-2 font-light'>
+                    {workspace?.messages[0]?.content}
+                </h2>
+            ))}
+        </div>
     </div>
   )
 }
